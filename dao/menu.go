dd33b42db87cedcaf2293723a98a4f6e32455422
yuyan <yuyan@50yc.com>
2024-05-09 17:51:00 +0800

    add：支持后端动态菜单

--- dao/menu.go
+++ dao/menu.go
@@ -17,6 +17,7 @@ type MenuItem struct {
 	Component string            `json:"component"`
 	Name      string            `json:"name"`
 	Meta      map[string]string `json:"meta"`
+	Children  []*MenuItem       `json:"children"`
 }
 
 // GetUserMenu 获取用户菜单
@@ -27,14 +28,14 @@ func (m *menu) GetUserMenu() (data []*MenuItem, err error) {
 		menuItems []*MenuItem
 	)
 
-	// 获取菜单列表
+	// 获取一级菜单
 	if err := global.MySQLClient.Find(&menus).Error; err != nil {
 		logger.Error("ERROR：", err.Error())
 		return nil, errors.New(err.Error())
 	}
 
 	for _, menu := range menus {
-		// 将菜单模型转换为返回给前端的格式
+		// 将一级菜单模型转换为返回给前端的格式
 		menuItem := &MenuItem{
 			Path:      menu.Path,
 			Component: menu.Component,
@@ -43,7 +44,31 @@ func (m *menu) GetUserMenu() (data []*MenuItem, err error) {
 				"title": menu.Title,
 				"icon":  menu.Icon,
 			},
+			Children: nil,
 		}
+
+		// 获取一级菜单对应的二级菜单
+		var subMenus []*model.SubMenu
+		if err := global.MySQLClient.Where("menu_id = ?", menu.Id).Find(&subMenus).Error; err != nil {
+			logger.Error("ERROR：", err.Error())
+			return nil, errors.New(err.Error())
+		}
+
+		for _, subMenu := range subMenus {
+			// 将二级菜单转换为返回给前端的格式
+			subMenuItem := &MenuItem{
+				Path:      subMenu.Path,
+				Component: subMenu.Component,
+				Name:      subMenu.Name,
+				Meta: map[string]string{
+					"title": subMenu.Title,
+					"icon":  subMenu.Icon,
+				},
+			}
+
+			menuItem.Children = append(menuItem.Children, subMenuItem)
+		}
+
 		menuItems = append(menuItems, menuItem)
 	}
 
